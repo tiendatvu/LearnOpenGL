9e635218340012107aabd4447ac0a8da76e09a60
Tien Dat Vu_G4 <tiendatvu93@gmail.com>
2020-11-23 00:09:27 +0700

    Change names of HelloTriangle and add Shader lession
    COMMIT DATE
        23 November 2020
    
    SUMMARY
        Change names of HelloTriangle and add Shader lession
    
    DESCRIPTION
        - Change names of HelloTriangle and add Shader lession
    
    NOTE
    
    ISSUE
    
    TODO

---
+++ OpenGLTut/src/2_Shaders/ShaderTask2.cpp
@@ -0,0 +1,131 @@
+//// Task2: Specify a horizontal offset via a uniform and move the triangle to the right side of the screen in the vertex shader using this offset value
+//
+//#include <glad/glad.h>
+//#include <GLFW/glfw3.h>
+//
+//#include "shaderClass/Shader.h"
+//#include <iostream>
+//#include <windows.h>
+//#include <string.h>
+//
+//void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+//void processInput(GLFWwindow* window);
+//
+//// settings
+//const unsigned int SCR_WIDTH = 800;
+//const unsigned int SCR_HEIGHT = 600;
+//
+//int main()
+//{
+//    // glfw init and configure
+//    glfwInit();
+//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+//
+//    // glfw window creation
+//    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "ShaderClass_LearnOpenGL", NULL, NULL);
+//    if (window == NULL)
+//    {
+//        std::cout << "Failed to create GLFW window" << std::endl;
+//        glfwTerminate();
+//        return -1;
+//    }
+//    glfwMakeContextCurrent(window);
+//    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+//
+//    // glad: load all OPENGL function pointers
+//    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+//    {
+//        std::cout << "Failed to initialize GLAD" << std::endl;
+//        return -1;
+//    }
+//
+//    // build and compile our shader program
+//    const unsigned long maxDir = 260;
+//    char currentDir[maxDir];
+//    GetCurrentDirectory(maxDir, currentDir);
+//
+//    std::string vsFile1 = "/src/2.Shaders/ShaderFiles/shader3.vs";
+//    std::string frsFile1 = "/src/2.Shaders/ShaderFiles/shader1.frs";
+//    Shader ourShader(currentDir + vsFile1, currentDir + frsFile1); // you can name your shader files however you like
+//
+//    // set up vertex data (and buffers) and configure vertex attributes
+//    float vertices[] = {
+//        // positions         // colors
+//         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+//        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+//         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+//    };
+//
+//    unsigned int VBO, VAO;
+//    glGenVertexArrays(1, &VAO);
+//    glGenBuffers(1, &VBO);
+//    // bind the vertex array object first, then bind and set vertex buffers, and then configure vertex attributes
+//    glBindVertexArray(VAO);
+//
+//    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+//    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+//
+//    // position attribute
+//    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+//    glEnableVertexAttribArray(0);
+//    // color attribute
+//    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+//    glEnableVertexAttribArray(1);
+//
+//    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+//    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+//    // glBindVertexArray(0);
+//
+//    // render loop
+//    while (!glfwWindowShouldClose(window))
+//    {
+//        // input
+//        processInput(window);
+//
+//        // render
+//        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+//        glClear(GL_COLOR_BUFFER_BIT);
+//
+//        // render the triangle
+//        ourShader.use();
+//        glBindVertexArray(VAO);
+//
+//        // update uniform
+//        float offset = 0.5f;
+//        ourShader.setFloat("xOffset", offset);
+//
+//        glDrawArrays(GL_TRIANGLES, 0, 3);
+//
+//        // glfw: swap buffers and poll IO envents
+//        glfwSwapBuffers(window);
+//        glfwPollEvents();
+//    }
+//
+//    // de-allocate all resources once they;'ve outlived their purpose
+//    glDeleteVertexArrays(1, &VAO);
+//    glDeleteBuffers(1, &VBO);
+//
+//    // glfw: terminate, clearing all previously allocated GLFW resources.
+//    // ------------------------------------------------------------------
+//    glfwTerminate();
+//    return 0;
+//}
+//
+//// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+//// ---------------------------------------------------------------------------------------------------------
+//void processInput(GLFWwindow *window)
+//{
+//    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+//        glfwSetWindowShouldClose(window, true);
+//}
+//
+//// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+//// ---------------------------------------------------------------------------------------------
+//void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+//{
+//    // make sure the viewport matches the new window dimensions; note that width and 
+//    // height will be significantly larger than specified on retina displays.
+//    glViewport(0, 0, width, height);
+//}
